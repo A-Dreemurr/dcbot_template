46c5a85d3f29deccf166c78f6a23f6a0d0861a94
A-Dreemurr <134613472+A-Dreemurr@users.noreply.github.com>
2023-07-04 17:02:18 +0800

    dcbot
    card, chat, dice

--- commands/chat.js
+++ commands/chat.js
@@ -1,3 +1,5 @@
+const { SlashCommandBuilder } = require("discord.js");
+
 const { Configuration, OpenAIApi } = require("openai");
 
 require("dotenv").config();
@@ -25,7 +27,7 @@ module.exports = {
                 },
             ],
         });
-        const responseMessage = '> ' + interaction.options.getString('input') + '\n' + response.data.choices[0].message.content;​
+        const responseMessage = '> ' + interaction.options.getString('input') + '\n' + response.data.choices[0].message.content;
         interaction.editReply(responseMessage);
     },
 };
--- commands/embed.js
+++ commands/embed.js
@@ -2,10 +2,27 @@ const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
 module.exports = {
     data: new SlashCommandBuilder().setName("embed").setDescription("show an embed"),
     async execute(client, interaction) {
-        const embed = new EmbedBuilder().setTitle("I'm an Embed");
+        const embed = new EmbedBuilder()
+            .setTitle("I'm an Embed")
+            .setColor("Random")
+            .setDescription('早上好中囯，现在我有冰激凌')
+            .addFields(
+                            {
+                                name: '两个礼拜以后',
+                                value: '因為非常好电影',
+                                inline: true
+                            },
+                            {
+                                name: '速度与激情9',
+                                value: '動作非常好',
+                                inline: true
+                            }
+                        )
         interaction.reply({ embeds: [embed] });
     },
 };
+
+    
 /*
 .setURL('放URL')
 .setAuthor({ name: '', iconURL: '放URL', url: '放URL' })
